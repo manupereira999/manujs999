03e5495dd3dae52a07c556fc79d4ba1e2beb2f42
manupereira999 <140511710+manupereira999@users.noreply.github.com>
2023-07-25 09:57:30 -0300

    upload arquivo

---
+++ sketch.js
@@ -0,0 +1,37 @@
+let cor
+let posicaoHorizontal;
+let posicaoVertical;
+
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+  
+}
+
+function draw() {
+  
+  
+ stroke("yellow") 
+  fill(cor)
+  circle(posicaoHorizontal,posicaoVertical, 20);
+  
+  if(mouseX < posicaoHorizontal) {
+    posicaoHorizontal = posicaoHorizontal - 3;
+    }
+
+  if(mouseX >posicaoHorizontal) {
+    posicaoHorizontal = posicaoHorizontal +3;
+ }
+
+  if(mouseY < posicaoHorizontal{
+    posicaoHorizontal = posicaoHorizontal--;
+  }
+  
+  f(mouseY>posicaoHorizontal) {
+    posicaoHorizontal = posicaoHorizontal++;
+  }
+}  
